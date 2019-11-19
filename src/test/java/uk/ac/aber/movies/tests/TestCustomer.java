913348c3954eaaec94875c93ad3146a55df0c6f6
Neil Taylor <neiltaylor@mac.com>
2019-11-19 06:34:51 +0000

    failed test

--- src/test/java/uk/ac/aber/movies/tests/TestCustomer.java
+++ src/test/java/uk/ac/aber/movies/tests/TestCustomer.java
@@ -33,7 +33,7 @@ public class TestCustomer {
 	
 	@Test 
 	public void shouldBeInitialisedWithSpecifiedName() { 
-		assertEquals("Sam Jones", customer.getName(), "Incorrect Name");
+		assertEquals("Samuel Jones", customer.getName(), "Incorrect Name");
 	}	
 	
 	@Test
